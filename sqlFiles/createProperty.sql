8a91f99ba610e708d36efe2f3fe808a44a3e4ff3
stevengeorge128 <stevengeorge@arizona.edu>
2025-05-04 18:39:36 -0700

    Updated sql files

---
+++ sqlFiles/createProperty.sql
@@ -0,0 +1,4 @@
+CREATE TABLE Property (
+    name    VARCHAR2(100) PRIMARY KEY,
+    type    VARCHAR2(50) NOT NULL
+);
---
+++ sqlFiles/sequenceToCreateTable.sql
@@ -0,0 +1,25 @@
+@createMember
+@createSkiPass
+@createSkiPassManualChangeLog
+@createArchivedPass
+
+@createProperty
+@createShuttle
+@createIncome
+
+@createLift 
+@createLiftLog
+@createTrail
+
+@createRental
+@createRentalChangeLog
+
+@createEquipment
+@createEquipmentChangeLog
+
+-- @createEmployee
+-- @createInstructor
+
+-- @createLesson
+-- @createLessonPurchase
+-- @createLessonLog 
